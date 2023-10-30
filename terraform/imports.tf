b7f05f71a4fec2540eaad707bba0e1f64e5eb0d5
Jim Martens <github@2martens.de>
2023-10-30 23:47:16 +0100

    Added imports for 2martens.eu

--- terraform/imports.tf
+++ terraform/imports.tf
@@ -12,3 +12,27 @@ import {
   to = inwx_domain_contact.admin
   id = "73763"
 }
+import {
+  to = inwx_nameserver_record.twomartens_eu_root_redirect
+  id = "2martens.eu:378503086"
+}
+import {
+  to = inwx_nameserver_record.twomartens_eu_www_redirect
+  id = "2martens.eu:378503087"
+}
+import {
+  to = inwx_nameserver_record.twomartens_eu_ns1
+  id = "2martens.eu:378502917"
+}
+import {
+  to = inwx_nameserver_record.twomartens_eu_ns2
+  id = "2martens.eu:378502918"
+}
+import {
+  to = inwx_nameserver_record.twomartens_eu_ns3
+  id = "2martens.eu:378502919"
+}
+import {
+  to = inwx_nameserver_record.twomartens_eu_soa
+  id = "2martens.eu:378502916"
+}
