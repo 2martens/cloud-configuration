027b5713eb3abde6bf2832ef8de1ddefe016eca5
Jim Martens <github@2martens.de>
2023-10-31 14:18:20 +0100

    Removed terraform

--- terraform/providers.tf
+++
@@ -1,9 +0,0 @@
-provider "inwx" {
-  api_url  = "https://api.domrobot.com/jsonrpc/"
-  username = var.inwx_user
-  password = var.inwx_password
-}
-
-provider "hcloud" {
-  token = var.hcloud_token
-}
