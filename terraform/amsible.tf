d7dc2de79e7956161dcfe6d3e61ee7bc7ee89743
Jim Martens <github@2martens.de>
2023-10-31 11:49:42 +0100

    Updated ansible output

--- terraform/amsible.tf
+++
@@ -1,6 +0,0 @@
-output "ansible_inventory" {
-  value = templatefile("${path.module}/inventory.tftpl", {
-    devops = hcloud_server.server_devops, k8s_servers = [hcloud_server.server_k8s_test]
-  })
-  depends_on = [hcloud_server.server_k8s_test, hcloud_server.server_k8s_test]
-}
