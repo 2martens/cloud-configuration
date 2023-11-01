dca124950fb205536337954a5530fe0ad3e5dcc6
Jim Martens <github@2martens.de>
2023-11-02 00:04:06 +0100

    Fixed path to repo for real

--- helm/scripts/release-package.sh
+++ helm/scripts/release-package.sh
@@ -14,7 +14,7 @@ do
   helm package --sign --key "github@2martens.de" --keyring ~/.gnupg/secring.gpg .
   mv -- *.tgz *.tgz.prov ~/git-repos/helm-charts/charts
 done
-cd ~/git-repos/helm-charts/charts || { echo "$HOME/git-repos/repo/charts/ does not exist"; exit 1; }
+cd ~/git-repos/repo/charts || { echo "$HOME/git-repos/repo/charts/ does not exist"; exit 1; }
 git pull
 git add .
 git commit -m "Upgraded $packages"
