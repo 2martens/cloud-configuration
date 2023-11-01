88571e5cdb70ced82f0b27de6f5660be6d6dc3b7
Jim Martens <github@2martens.de>
2023-11-02 00:08:30 +0100

    Fixed third path

--- helm/scripts/release-package.sh
+++ helm/scripts/release-package.sh
@@ -12,7 +12,7 @@ do
   fi
   cd "$HOME/git-repos/cloud-configuration/helm/$package" || { echo "Cannot find $package under $HOME/git-repos/cloud-configuration/helm/"; exit 1; }
   helm package --sign --key "github@2martens.de" --keyring ~/.gnupg/secring.gpg .
-  mv -- *.tgz *.tgz.prov ~/git-repos/helm-charts/charts
+  mv -- *.tgz *.tgz.prov ~/git-repos/repo/charts
 done
 cd ~/git-repos/repo/charts || { echo "$HOME/git-repos/repo/charts/ does not exist"; exit 1; }
 git pull
