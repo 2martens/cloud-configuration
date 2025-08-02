04a25ed27dc881245b4fb4bfab8099a859ddc827
Jim Martens <github@2martens.de>
2025-08-02 23:08:36 +0200

    fix: Create missing directory

--- docker/test/nginx/entrypoint.sh
+++ docker/test/nginx/entrypoint.sh
@@ -8,6 +8,7 @@ if [ -f "$CERT_PATH" ] && [ -f "$KEY_PATH" ]; then
   echo "✅ Real certificate found — starting full HTTPS Nginx"
 else
   echo "⚠️ No certificate found — starting Nginx with self-signed TLS fallback"
+  mkdir -p /etc/letsencrypt/live/api.2martens.de
   ln -sf /etc/nginx/ssl/selfsigned.crt /etc/letsencrypt/live/api.2martens.de/fullchain.pem
   ln -sf /etc/nginx/ssl/selfsigned.key /etc/letsencrypt/live/api.2martens.de/privkey.pem
 fi
